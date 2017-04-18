2f27a4c4435f898ebe9f78cf0c9c11a293ae9ffe
vinist <vinicius.sturaro@gmail.com>
2017-04-17 23:33:02 -0400

    mysql

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,11 +1,17 @@
 package com.algaworks.cobranca.controller;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.enuns.StatusTituloEnum;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.ITitulos;
 
@@ -17,8 +23,9 @@ public class TituloController {
 	private ITitulos iTitulos;
 	
 	@RequestMapping("/novo")
-	public String novo(){
-		return "cadastroTitulo";
+	public ModelAndView novo(){
+		ModelAndView mv = new ModelAndView("cadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -27,6 +34,11 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("cadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
 		return mv;
+	}
+	
+	@ModelAttribute("listStatusTitulo")
+	public List<StatusTituloEnum> getListStatusTitulo(){
+		return Arrays.asList(StatusTituloEnum.values());
 	}
 
 }
