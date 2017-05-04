9744d524f3bb4498983babeef81a3c13ce4c6817
vinist <vinicius.sturaro@gmail.com>
2017-05-03 22:09:58 -0400

    datepicker

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -38,10 +39,15 @@ public class TituloController {
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes){
 		if(errors.hasErrors())
 			return CADASTRO_VIEW;
+		try {
+			iTitulos.save(titulo);
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch (DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, "Formato de data inválido!");
+			return CADASTRO_VIEW;
+		}
 		
-		iTitulos.save(titulo);
-		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
-		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
