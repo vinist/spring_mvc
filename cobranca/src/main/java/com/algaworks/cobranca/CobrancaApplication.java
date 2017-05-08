489048b3e9d1e023c3d25b3b5ff54375e3cb7368
vinist <vinicius.sturaro@gmail.com>
2017-05-07 22:41:31 -0400

    parte final

--- cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -4,8 +4,12 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -18,5 +22,16 @@ public class CobrancaApplication {
 	@Bean
 	public LocaleResolver localResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	@Configuration
+	public static class Config extends WebMvcConfigurerAdapter {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+		
+		
 	}
 }
